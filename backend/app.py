28eff1a2ada99579327f57f723e32c786aaa044e
theryanfo <ryan.s.fordham@gmail.com>
2024-11-22 17:53:24 -0800

    artistsYouLike and is_logged_in routes

--- backend/app.py
+++ backend/app.py
@@ -10,18 +10,18 @@ import random
 load_dotenv()
 MAIN_REDIRECT_URL = "http://localhost:5173"
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=MAIN_REDIRECT_URL)
+# CORS(app)
 
 app.secret_key = os.getenv("SECRET_KEY")
 app.config['SESSION_COOKIE_NAME'] = 'spotify-keepup-session'
 TOKEN_INFO = "token_info"
 
 
-@app.after_request
-def apply_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    return response
+# @app.after_request
+# def apply_cors_headers(response):
+#     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
+#     response.headers['Access-Control-Allow-Credentials'] = 'true'
+#     return response
 
 # App Routes
 
@@ -35,6 +35,16 @@ def test():
 def logout():
     session.clear()
     return jsonify({"message": "Logged out successfully"})
+
+
+@app.route('/is_logged_in')
+def is_logged_in():
+    print(session)
+    if (TOKEN_INFO in session):
+        print("logged_in: True")
+        return jsonify({"logged_in": True})
+    print("logged_in: False")
+    return jsonify({"logged_in": False})
 
 
 @app.route('/login')
@@ -57,15 +67,89 @@ def redirectPage():
     else:
         print("NO SESSION TOKEN FOUND")
 
-    response = make_response(jsonify({'url': 'http://localhost:5173/'}))
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    return response
+    return jsonify({"message": "redirect finished"})
+    # response = make_response(jsonify({'url': 'http://localhost:5173/'}))
+    # return response
     # return redirect(os.getenv("MAIN_REDIRECT_URL"))
     # return jsonify({'url': 'http://localhost:5173/'})
 
 
+@app.route('/artistsYouLike')
+def artistsYouLike():
+    try:
+        token_info = get_token()
+    except:
+        print("USER NOT LOGGED IN")
+        return redirect("/login")
+    
+    sp = spotipy.Spotify(auth=token_info['access_token'])
 
+    # user_id = sp.current_user()['id']
+
+    # get artists from current user top tracks
+    trendingArtists = set()
+    for track in sp.current_user_top_tracks(limit=30, offset=0)['items']:
+        for artist in track['artists']:
+            trendingArtists.add(artist['id'])
+
+    # get most diff. songs listened to artists all-time and 
+    # already-listened to songs (for exclusion)
+    topArtists = {}
+    topTracks = []
+    i = 0
+    while True and i < 20:
+        tracks = sp.current_user_top_tracks(limit=50, offset=50 * i)['items']
+        i += 1
+        for track in tracks:
+            artists = []
+            for artist in track['artists']:
+                artists += [(artist['id'], artist['name'])]
+                topArtists[artist['id']] = 1 + topArtists.get(artist['id'], 0)
+
+            topTracks += [(track['name'], track['id'], 
+                           track['album']['images'][2]['url'], artists)]
+            
+        if (len(tracks) < 50):
+            break
+
+    # get songs from listened-to artists
+    topArtists = sorted(topArtists.items(), key=lambda item: item[1], reverse=True)
+
+    # create final artist list (3 steps)
+    # 1. up to 5 artists from sample of most-listened to artists
+    artistsToUse = set()
+    topTenth = len(topArtists) // 10
+    if topTenth >= 5:
+        for artist in random.sample(topArtists[:topTenth], 5):
+            artistsToUse.add(artist[0])
+    else:
+        artistsToUse.update(artist for artist in topArtists[:5]) 
+
+    # 2. up to 5 artists from recently Liked songs
+    startSize = len(artistsToUse)
+    items = sp.current_user_saved_tracks(limit=50, offset=0)['items']
+    random.shuffle(items)
+    for item in items:
+        for artist in item['track']['artists']:
+            artistsToUse.add(artist['id'])
+            if len(artistsToUse) >= startSize + 5:
+                break
+        if len(artistsToUse) >= startSize + 5:
+            break
+
+    # 3. fill rest up to 20 from artists from user current high frequency tracks
+    while len(artistsToUse) < 20:
+        for track in topTracks:
+            for artist in track[3]:
+                artistsToUse.add(artist[0])
+                if len(artistsToUse) >= 20:
+                    break
+            if len(artistsToUse) >= 20:
+                break
+    
+    artists_to_use = list(artistsToUse) 
+    
+    return jsonify(artists_to_use)
 
 
 # Helper Functions
@@ -78,6 +162,19 @@ def create_spotify_oauth():
         scope="user-library-read,user-top-read,user-follow-read,playlist-read-private,playlist-read-collaborative,playlist-modify-public,playlist-modify-private"
     )
 
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    # sp_oauth = create_spotify_oauth()
+    # token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    return token_info
 
 # Run Application
 
